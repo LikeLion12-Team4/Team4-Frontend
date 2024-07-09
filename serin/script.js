be2acd3040466f2f5742c39665034cdfe908eaae
serein <gims37340@gmail.com>
2024-07-09 20:00:23 +0900

    메인/헤더 수정, 웹 푸시 구현
    - index.html 수정
    - style.css 수정
    - script.js 수정
    - push.html,css 생성

--- serin/script.js
+++ serin/script.js
@@ -14,3 +14,68 @@ window.addEventListener('load', function() {
         }
     });
 });
+
+// like_btn
+document.addEventListener('DOMContentLoaded', function() {
+    // 하트 아이콘 클릭 이벤트 처리
+    document.querySelectorAll('.fa-heart').forEach(function(heart) {
+        heart.addEventListener('click', function() {
+            this.classList.toggle('active');
+            if (this.classList.contains('active')) {
+                this.classList.remove('fa-regular');
+                this.classList.add('fa-solid');
+                this.style.color = "#6266ED";
+            } else {
+                this.classList.remove('fa-solid');
+                this.classList.add('fa-regular');
+                this.style.color = "";
+            }
+        });
+    });
+
+    // 비디오 슬라이드 기능 구현
+    const sliders = document.querySelectorAll('.video_silder');
+    const videoWidth = 243;
+
+    sliders.forEach(function(slider) {
+        const leftBtn = slider.querySelector('.left_btn');
+        const rightBtn = slider.querySelector('.right_btn');
+        const videoContainer = slider.querySelector('.video_container');
+        let scrollAmount = 0;
+
+        leftBtn.addEventListener('click', function() {
+            scrollAmount = Math.max(scrollAmount - videoWidth, 0);
+            videoContainer.scrollTo({
+                top: 0,
+                left: scrollAmount,
+                behavior: 'smooth'
+            });
+        });
+
+        rightBtn.addEventListener('click', function() {
+            scrollAmount = Math.min(scrollAmount + videoWidth, videoContainer.scrollWidth - videoContainer.clientWidth);
+            videoContainer.scrollTo({
+                top: 0,
+                left: scrollAmount,
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    //dot
+    const slides = document.querySelectorAll(".video_container");
+    const dots = document.querySelectorAll(".dot");
+
+    dots.forEach((dot, index) => {
+        dot.addEventListener("click", function() {
+            dot.firstChild.classList.remove("active");
+            this.classList.add("active");
+
+            slides.forEach((slide, slideIndex) => {
+                slide.style.transform = 'translateX(-${index *100}%';
+            });
+        });
+    });
+    
+});
+
