032c926e28089687b2c6e59397c1c04ba24f872d
seungyeons2 <seungyeonsi913@gmail.com>
2024-07-10 16:10:28 +0900

    fix: 폴더링 후 경로 수정, 헤더 폭 고정(임시)

---
+++ js/pages/mypage.js
@@ -0,0 +1,23 @@
+// include.js
+window.addEventListener("load", function () {
+  var allElements = document.getElementsByTagName("*");
+  Array.prototype.forEach.call(allElements, function (el) {
+    var includePath = el.dataset.includePath;
+    if (includePath) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+          el.outerHTML = this.responseText;
+        }
+      };
+      xhttp.open("GET", includePath, true);
+      xhttp.send();
+    }
+  });
+});
+
+document // '펼치기' -> 내 프로필 페이지로 이동
+  .getElementById("profile-button")
+  .addEventListener("click", function () {
+    window.location.href = "../../html/pages/profile.html";
+  });
