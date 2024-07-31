49e5faadad694b98c1392a364018c70106b957c0
yejin7457 <161455449+yejin7457@users.noreply.github.com>
2024-08-01 01:10:11 +0900

    feat : 네이버 로그인 완성 & 로그아웃 기능 구현

--- js/pages/header.js
+++ js/pages/header.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   var isLogin = sessionStorage.getItem("isLogin");
+
   function change() {
     console.log(isLogin);
     if (isLogin == "true") {
@@ -14,7 +15,9 @@ document.addEventListener("DOMContentLoaded", function () {
       document.querySelector(".welcome_msg").style.visibility = "hidden";
     }
   }
+
   change();
+
   function getCookie(name) {
     var nameEQ = name + "=";
     var cookies = document.cookie.split(";");
@@ -29,11 +32,18 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     return null;
   }
-  
+
+  function deleteCookie(name) {
+    var domain = window.location.hostname;
+    document.cookie = name + "=; Max-Age=-99999999; path=/; domain=" + domain;
+  }
+
   function getToken() {
     return getCookie("accessToken") || null;
   }
-  var name ='';
+
+  var name = "";
+
   function getUserInfo() {
     var requestOptions = {
       method: "GET",
@@ -42,33 +52,35 @@ document.addEventListener("DOMContentLoaded", function () {
       },
       redirect: "follow",
     };
-  
+
     fetch("http://3.37.18.8:8000/users/user/", requestOptions)
       .then((response) => response.json())
       .then((result) => {
         name = result.username;
-        document.querySelector('.user_name').innerText = name;
-        console.log(name)
-        
+        document.querySelector(".user_name").innerText = name;
+        console.log(name);
       })
       .catch((error) => console.log("error", error));
   }
-  getUserInfo();
-});
 
-window.addEventListener("load", function () {
-  var allElements = document.getElementsByTagName("*");
-  Array.prototype.forEach.call(allElements, function (el) {
-    var includePath = el.dataset.includePath;
-    if (includePath) {
-      var xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-          el.outerHTML = this.responseText;
-        }
-      };
-      xhttp.open("GET", includePath, true);
-      xhttp.send();
-    }
-  });
+  getUserInfo();
+
+  function logout() {
+    // 모든 경로와 도메인에 대해 쿠키 삭제
+    deleteCookie("accessToken");
+    deleteCookie("refreshToken");
+
+    sessionStorage.removeItem("isLogin");
+    sessionStorage.clear();
+
+    // 디버깅: 로그아웃 후 쿠키 상태 확인
+    console.log("accessToken after logout: ", getCookie("accessToken"));
+    console.log("refreshToken after logout: ", getCookie("refreshToken"));
+
+    // 로그인 페이지로 이동합니다.
+    window.location.replace("login.html");
+  }
+
+  // 로그아웃 버튼에 이벤트 리스너를 추가합니다.
+  document.querySelector(".logout_btn").addEventListener("click", logout);
 });
